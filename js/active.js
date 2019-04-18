ea6f25a4bf5b078d02f0ea3eb1221775dcb916d4
coderlisan <coderlisan@gmail.com>
2019-04-18 22:17:44 +0600

    header and slider complete

--- js/active.js
+++ js/active.js
@@ -1,36 +1,14 @@
-    // abcd-active
-    $('.abcd-active').owlCarousel({
-        responsiveClass: true,
-        nav: false,
-        loop: true,
-        dots: false,
-        margin: 50,
-        responsive: {
-            576: {
-                items: 2,
-            },
-            768: {
-                items: 3,
-            },
-            992: {
-                items: 4,
-            },
-            1200: {
-                items: 6,
-            }
-        }
-    })
+$(document).ready(function () {
 
-    // magnify-active
-    $('.video-popup').magnificPopup({ type: 'iframe' })
-    
-    
-    // master-menu-active
-    jQuery('header nav').meanmenu({
-        
-        
-        
+//   slick-slider-area
+    $('.slider-active').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:false,
+        dots:false
     });
-    
+
+
     // function-last-brackets
 })
