f58e6917e001bcb2754a400f5b76d42fab80ea45
coderlisan <coderlisan@gmail.com>
2019-04-23 14:59:19 +0600

    add all thing it's responsive

--- js/active.js
+++ js/active.js
@@ -1,36 +1,63 @@
 $(document).ready(function () {
 
-//   slick-slider-area
+
+    $('#mobile-menu-active').meanmenu({
+        meanScreenWidth: "767",
+        meanMenuContainer: '.mobile-menu',
+    });
+
+
+
+
+
+    //   slick-slider-area
     $('.slider-active').slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows:false,
-        dots:false
+        arrows: false,
+        dots: false
     });
-// counter-area
-$('.counter').counterUp({
-    delay: 10,
-    time: 5000
-});
+    // counter-area
+    $('.counter').counterUp({
+        delay: 10,
+        time: 5000
+    });
 
 
-//   testimonial-slider-area
+    //   testimonial-slider-area
     $('.testimonial-active').slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows:false,
-        dots:true
+        arrows: false,
+        dots: true
     });
-    
+
     //   brand-area
     $('.brand-active').slick({
         infinite: true,
         slidesToShow: 5,
         slidesToScroll: 2,
-        arrows:false,
-        dots:false
+        margin: 10,
+        arrows: false,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 
     // function-last-brackets
